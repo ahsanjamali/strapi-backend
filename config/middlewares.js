8fa6db2dbf33cd4d098dab67998755e862242725
ahsanjamali <ahsanjamali7042@gmail.com>
2024-12-09 21:28:43 +0500

    Initial commit with .gitignore

---
+++ config/middlewares.js
@@ -0,0 +1,18 @@
+module.exports = [
+  'strapi::logger',
+  'strapi::errors',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      headers: '*',
+      origin: ['http://localhost:3000']
+    }
+  },
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+]; 
